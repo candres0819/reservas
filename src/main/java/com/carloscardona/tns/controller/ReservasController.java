fe3b1ced6a844c0c88edb77ab6a858455dfdc3ff
Carlos Andrés Cardona M <candres0819@gmail.com>
2016-05-31 00:59:51 -0500

    Signed-off-by: Carlos Andrés Cardona M <candres0819@gmail.com>

--- src/main/java/com/carloscardona/tns/controller/ReservasController.java
+++ src/main/java/com/carloscardona/tns/controller/ReservasController.java
@@ -3,13 +3,21 @@ package com.carloscardona.tns.controller;
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.carloscardona.tns.dao.UsuarioRepository;
+import com.carloscardona.tns.model.Usuario;
 
 /**
  * 
@@ -23,15 +31,32 @@ public class ReservasController {
 	UsuarioRepository usuarioRepository;
 
 	@RequestMapping("/resource")
-	public Map<String, Object> home() {
+	public Map<String, Object> home(HttpServletRequest request) {
+		Principal user = request.getUserPrincipal();
 		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
+		model.put("id", user.getName());
 		return model;
 	}
 
 	@RequestMapping("/user")
 	public Principal user(Principal user) {
 		return user;
+	}
+
+	@RequestMapping(value = "/registrar", method = RequestMethod.POST)
+	public ResponseEntity<Void> registrar(@RequestBody Usuario user, UriComponentsBuilder ucBuilder) {
+		System.out.println(user.toString());
+		if (isUserExist(user)) {
+			System.out.println("A User with name " + user.getUserName() + " already exist");
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		usuarioRepository.save(user);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(ucBuilder.path("/usuario/{id}").buildAndExpand(user.getId()).toUri());
+		return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	}
+
+	private boolean isUserExist(Usuario user) {
+		return usuarioRepository.findByUserName(user.getUserName()) != null;
 	}
 }
--- src/main/java/com/carloscardona/tns/dao/UsuarioRepository.java
+++ src/main/java/com/carloscardona/tns/dao/UsuarioRepository.java
@@ -4,6 +4,7 @@
 package com.carloscardona.tns.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.carloscardona.tns.model.Usuario;
 
@@ -11,6 +12,7 @@ import com.carloscardona.tns.model.Usuario;
  * @author candr
  *
  */
+@RestController
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	Usuario findByUserName(String username);
